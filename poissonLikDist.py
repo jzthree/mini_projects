5905ed1fbdcdf997c9c8e535011924301f6cfc5e
Jian Zhou <jzthree@users.noreply.github.com>
2017-06-19 01:31:11 -0400

    handling zeros

--- poissonLikDist.py
+++ poissonLikDist.py
@@ -1,31 +1,46 @@
 import numpy as np
 
 #Measuring Poisson likelihood ratio distance between two normalized count vectors
-#In contrast to normal Euclidean distances, this measure considers uncertainty 
-#of measurements so lower counts generally gives higher distances
+#In contrary to normal Euclidean distances, this measure considers uncertainty
+#of measurements so lower counts gives higher distances
 #Note: this is not a metric (doesn't satisfy triangle inequality)
 
 
 def poissonLikDist(a,b):
     asum = np.sum(a)
     bsum = np.sum(b)
-    
+
+
     #MLE expectation of a and b assuming they are drawn from the same distribution
     Ea  = (a+b)*asum/(asum+bsum)
     Eb  = Ea*bsum/asum
+    if np.any(a==0):
+        #expectation based on Poisson approximation
+        a[a==0] =  -np.log(np.mean(a==0))
+    if np.any(b==0):
+        b[b==0] =  -np.log(np.mean(b==0))
 
     logp_unnormalized_adist  = -a + Ea + a*(np.log(a)-np.log(Ea))
     logp_unnormalized_bdist  = -b + Eb + b*(np.log(b)-np.log(Eb))
     return np.sum(logp_unnormalized_adist + logp_unnormalized_bdist)
 
 def poissonLikDistmv(a,b):
-    #a is matrix, b is a row vector
+    #a is matrix, b is a row vector. Note implementation is different from
+    #poissonLikDist for handling zeros
     asum = np.sum(a, axis=1)
     bsum = np.sum(b)
-    
+
     #MLE expectation of a and b assuming they are drawn from the same distribution
     Eb  = (a+b[np.newaxis,:])*(bsum/(asum+bsum))[:,np.newaxis]
     Ea  = Eb*asum[:,np.newaxis]/bsum
+
+    #expectation based on Poisson approximation.
+    if np.any(a==0):
+        zeroexp =  -np.log(np.mean(a==0, axis=1))
+        a[:,np.isfinite(zeroexp)] = a[:,np.isfinite(zeroexp)] + zeroexp[np.isfinite(zeroexp)]
+    if np.any(b==0):
+        zeroexp =  -np.log(np.mean(b==0, axis=1))
+        b[:,np.isfinite(zeroexp)] = b[:,np.isfinite(zeroexp)] + zeroexp[np.isfinite(zeroexp)]
 
     logp_unnormalized_adist  = -a + Ea + a*(np.log(a)-np.log(Ea))
     logp_unnormalized_bdist  = -b + Eb + b*(np.log(b)-np.log(Eb))
@@ -33,6 +48,7 @@ def poissonLikDistmv(a,b):
 
 def test_example():
     print poissonLikDist(np.asarray([1.,10.]),np.asarray([1.,10.]))
+    print poissonLikDist(np.asarray([1.,10.]),np.asarray([0.,10.]))
     print poissonLikDist(np.asarray([1.,10.]),np.asarray([1.,9.]))
     print poissonLikDist(np.asarray([1.,10.]),np.asarray([1.,5.]))
     print poissonLikDist(np.asarray([1.,10.]),np.asarray([1.,20.]))
@@ -42,3 +58,6 @@ def test_example():
     print poissonLikDist(np.asarray([10.,100.]),np.asarray([1.,9.]))
     print poissonLikDist(np.asarray([10.,100.]),np.asarray([1.,5.]))
     print poissonLikDist(np.asarray([10.,100.]),np.asarray([1.,20.]))
+
+    print poissonLikDistmv(np.asarray([[1,10],[10.,100.]]),np.asarray([1.,9.]))
+    print poissonLikDistmv(np.asarray([[0,10],[10.,100.]]),np.asarray([1.,9.]))
